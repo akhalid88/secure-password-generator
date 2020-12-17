3cb19095cf321ef1e94011ba94b6927a60481bf9
Muhammad A Khalid <akhalid.code@gmail.com>
2020-12-17 00:18:22 -0800

    add additional pseudo code and comments

--- script.js
+++ script.js
@@ -8,18 +8,20 @@ var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numeric = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var special = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
-
+var masterArray = [];
 console.log(lowercase);
 console.log(uppercase);
 console.log(numeric);
 console.log(special);
 console.log(special[23]);
 
-var tempArray = lowercase.concat(uppercase);
-tempArray = tempArray.concat(numeric);
-tempArray = tempArray.concat(special);
-console.log(tempArray);
-console.log(tempArray.length);
+console.log(masterArray);
+masterArray = masterArray.concat(lowercase);
+masterArray = masterArray.concat(uppercase);
+masterArray = masterArray.concat(numeric);
+masterArray = masterArray.concat(special);
+console.log(masterArray);
+console.log(masterArray.length);
 
 
 //END TEST CODE
@@ -31,6 +33,11 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  //  Variablles needed
+  // array of each criteria ?possible object? 
+  //
+
 
   //============
   // GIVEN I need a new, secure password
@@ -61,11 +68,26 @@ function writePassword() {
   //   special: false
   // }
   //chooseLowercase, chooseUppercase, chooseNumeric, chooseSpecial
-  //
-  // if chooseLowercase true
+  //if chooseLowercase true
   //criteria.lowercase = true
   //masterArray = masterArray.concat(lowercase)
-  //else
+  //
+  //if chooseUppercase true
+  //criter.uppercase = true
+  //masterArray = masterArray.concat(uppercase)
+  //
+  //if chooseNumeric true
+  //criteria.numeric 
+  //masterArray = masterArray.concat(numeric)
+  //
+  //if chooseSpecial true
+  //criteria.special 
+  //masterArray = masterArray.concat(special)
+  //
+  //check to see if masterArray is null
+  //if chiseCriteria = 0 restart loop?
+
+  //research logical NAND / NOR 
 
 
   // WHEN I answer each prompt
@@ -77,12 +99,21 @@ function writePassword() {
   // WHEN all prompts are answered
   // THEN a password is generated that matches the selected criteria
   //with each selection of 
+  //random number times the lenght of the array
+  //Math.random() * masterArray.length();
+
+
+  // while currentLetter < passwordLength
+  //   
+  //   num = Math.floor(Math.random() * 94);
+  //   pwString = pwString.concat(masterArray[num])
+  //   currentLetter++;
+  // 
+  // }
 
   // WHEN the password is generated
   // THEN the password is either displayed in an alert or written to the page
   //alert(generatePassword());
-
-
 }
 
 // Add event listener to generate button
